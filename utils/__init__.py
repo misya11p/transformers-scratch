a400d51975866e1dc75c28c0274693b9d76998b5
komiya <90894519+misya11p@users.noreply.github.com>
2025-10-13 01:18:57 +0900

    ”

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,10 @@
 from ._config import load_config
 from ._tokenizer import get_tokenizer, train_tokenizer
+from ._dataset import (
+    DNAME_TEXTS,
+    DNAME_TOKENS,
+    FNAME_PARQUET_TRAIN,
+    FNAME_PARQUET_VALID,
+)
 from ._dataset import get_dataloader, format_text
 from ._generate import Generator
