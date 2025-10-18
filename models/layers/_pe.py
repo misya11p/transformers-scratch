85ecf353f9ddce89b1e64e050fb20657bca40b8c
komiya <90894519+misya11p@users.noreply.github.com>
2025-10-18 16:08:58 +0900

    add: rope

--- models/layers/_pe.py
+++ models/layers/_pe.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 
 
 class SinusoidalPositionalEmbedding(nn.Module):
-    def __init__(self, d_model, max_len=1024, base=10000):
+    def __init__(self, d, max_len=1024, base=10_000):
         super().__init__()
         assert max_len % 2 == 0, "max_len must be even"
-        pos = torch.arange(max_len)
-        dim = torch.arange(0, d_model, 2)
-        pe = torch.zeros(max_len, d_model)
-        pe[:, ::2] = torch.sin(pos.unsqueeze(-1) / base ** (dim / d_model))
-        pe[:, 1::2] = torch.cos(pos.unsqueeze(-1) / base ** (dim / d_model))
+        p = torch.arange(max_len)
+        i = torch.arange(0, d, 2)
+        pe = torch.zeros(max_len, d)
+        pe[:, ::2] = torch.sin(p.unsqueeze(-1) / base ** (i / d))
+        pe[:, 1::2] = torch.cos(p.unsqueeze(-1) / base ** (i / d))
         self.register_buffer("pe", pe)
 
     def forward(self, x):
@@ -24,3 +24,27 @@ class LearnablePositionalEmbedding(nn.Module):
 
     def forward(self, x):
         return x + self.pe[:x.size(-2)]
+
+
+class RotaryPositionalEmbedding(nn.Module):
+    def __init__(self, d, max_len=4096, base=10_000):
+        super().__init__()
+        p = torch.arange(max_len)
+        i = torch.arange(0, d, 2)
+        theta = p.unsqueeze(-1) / (base ** (i / d))
+        self.sin = torch.sin(theta)
+        self.cos = torch.cos(theta)
+        self.idx = torch.arange(d)
+        self.d = d
+
+    def _get_rotation_matrix(self, l):
+        R = torch.zeros(l, self.d, self.d)
+        R[:, self.idx, self.idx] = self.cos[:l].repeat_interleave(2, -1)
+        R[:, self.idx[1::2], self.idx[::2]] = self.sin[:l]
+        R[:, self.idx[::2], self.idx[1::2]] = -self.sin[:l]
+        return R
+
+    def forward(self, x):
+        R = self._get_rotation_matrix(x.size(1)).to(x.device)
+        x = torch.einsum("blhd,lvd->blhv", x, R)
+        return x
