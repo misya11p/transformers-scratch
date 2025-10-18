b1a21af1221dba1ef128af0848bf014639b59725
komiya <90894519+misya11p@users.noreply.github.com>
2025-10-18 15:56:57 +0900

    add base arg

--- models/layers/_pe.py
+++ models/layers/_pe.py
@@ -3,14 +3,14 @@ import torch.nn as nn
 
 
 class SinusoidalPositionalEmbedding(nn.Module):
-    def __init__(self, d_model, max_len):
+    def __init__(self, d_model, max_len=1024, base=10000):
         super().__init__()
         assert max_len % 2 == 0, "max_len must be even"
         pos = torch.arange(max_len)
         dim = torch.arange(0, d_model, 2)
         pe = torch.zeros(max_len, d_model)
-        pe[:, ::2] = torch.sin(pos.unsqueeze(-1) / 10000 ** (dim / d_model))
-        pe[:, 1::2] = torch.cos(pos.unsqueeze(-1) / 10000 ** (dim / d_model))
+        pe[:, ::2] = torch.sin(pos.unsqueeze(-1) / base ** (dim / d_model))
+        pe[:, 1::2] = torch.cos(pos.unsqueeze(-1) / base ** (dim / d_model))
         self.register_buffer("pe", pe)
 
     def forward(self, x):
@@ -18,7 +18,7 @@ class SinusoidalPositionalEmbedding(nn.Module):
 
 
 class LearnablePositionalEmbedding(nn.Module):
-    def __init__(self, d_model, max_len):
+    def __init__(self, d_model, max_len=1024):
         super().__init__()
         self.pe = nn.Parameter(torch.randn((max_len, d_model)))
 
