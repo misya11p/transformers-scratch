d0ec103b9cf3c8757f2d1654abf00c2ffb09f640
misya11p <90894519+misya11p@users.noreply.github.com>
2025-12-10 15:00:13 +0900

    modify: utilsは戻した

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,4 @@
-from .utils import get_model, load_model
+from ._utils import get_model, load_model
 from .vanilla_transformer import VanillaTransformer
 from .gpt2 import GPT2Encoder, GPT2
 from .vision_transformer import VisionTransformerEncoder, VisionTransformer
