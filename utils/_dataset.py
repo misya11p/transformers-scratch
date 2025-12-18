70e5fca31f3fb81757ed1a45f55574e50d0a1dcc
misya11p <90894519+misya11p@users.noreply.github.com>
2025-12-18 23:34:29 +0900

    update: ds

--- utils/_dataset.py
+++ utils/_dataset.py
@@ -1,94 +1,68 @@
-from pathlib import Path
-import ast
-
-from datasets import Dataset, load_dataset
-from transformers import DataCollatorForLanguageModeling
-from torch.utils.data import DataLoader, DistributedSampler
-from tqdm import tqdm
+from datasets import load_dataset
+from torch.utils.data import IterableDataset, DataLoader
 
 from ._tokenizer import get_tokenizer
 
 
-DNAME_TEXTS = "texts"
-DNAME_TOKENS = "tokens"
-FNAME_PARQUET_TRAIN = "train.parquet"
-FNAME_PARQUET_VALID = "validation.parquet"
+class TextDataset(IterableDataset):
+    def __init__(self, ds):
+        self.ds = ds
+
+    def __iter__(self):
+        for text in self.ds["text"]:
+            yield text
 
 
 def get_dataloader(
     batch_size,
-    dpath_data="data/",
+    max_length=1024,
     tokenizer="tokenizer.json",
     world_size=1,
     rank=None,
 ):
-    dpath_data = Path(dpath_data)
-    dpath_tokens = dpath_data / DNAME_TOKENS
-
-    ds_train = load_dataset(str(dpath_tokens), split="train")
-    ds_valid = load_dataset(str(dpath_tokens), split="validation")
-
     if isinstance(tokenizer, str):
         tokenizer = get_tokenizer(tokenizer)
 
-    collater = DataCollatorForLanguageModeling(
-        tokenizer=tokenizer,
-        mlm=False,
+    def collate_fn(batch):
+        return tokenizer(
+            batch,
+            truncation=True,
+            max_length=max_length,
+            padding=True,
+            return_tensors="pt",
+        )["input_ids"]
+
+    ds_train = load_dataset(
+        "hotchpotch/fineweb-2-edu-japanese",
+        "small_tokens_cleaned",
+        split="train",
+        streaming=True,
     )
+    ds_valid = load_dataset(
+        "hotchpotch/fineweb-2-edu-japanese",
+        "small_tokens_cleaned",
+        split="test",
+        streaming=True,
+    )
+    ds_train = ds_train.shuffle(buffer_size=100)
 
     if world_size >= 2 and rank is not None:
-        batch_size = batch_size // world_size
-        train_sampler = DistributedSampler(
-            ds_train,
-            num_replicas=world_size,
-            rank=rank,
-            shuffle=True,
-            drop_last=True,
-        )
-        valid_sampler = DistributedSampler(
-            ds_valid,
-            num_replicas=world_size,
-            rank=rank,
-            shuffle=False,
-            drop_last=False,
-        )
-    else:
-        train_sampler = None
-        valid_sampler = None
+        ds_train = ds_train.shard(num_shards=world_size, index=rank)
+        ds_valid = ds_valid.shard(num_shards=world_size, index=rank)
 
     train_loader = DataLoader(
-        ds_train,
+        TextDataset(ds_train),
         batch_size=batch_size,
-        sampler=train_sampler,
-        shuffle=(train_sampler is None),
+        collate_fn=collate_fn,
         pin_memory=True,
-        drop_last=True,
-        collate_fn=collater,
+        num_workers=4,
     )
     valid_loader = DataLoader(
-        ds_valid,
+        TextDataset(ds_valid),
         batch_size=batch_size,
-        sampler=valid_sampler,
-        shuffle=False,
+        collate_fn=collate_fn,
         pin_memory=True,
-        drop_last=False,
-        collate_fn=collater,
+        num_workers=4,
     )
+
     return train_loader, valid_loader
-
-
-def format_text(ds):
-    sentences = []
-    texts = ds["text"]
-    for text in tqdm(texts, desc="Formatting text"):
-        decoded_string = ast.literal_eval(text).decode("utf-8")
-        sections = decoded_string.split("_START_SECTION_")
-        for section in sections:
-            paragraph = section.split("_START_PARAGRAPH_")[-1]
-            paragraph = paragraph.replace("_NEWLINE_", "")
-            paragraph = paragraph.replace("\n", "")
-            paragraph = paragraph.strip()
-            if paragraph:
-                sentences.append(paragraph)
-    ds_new = Dataset.from_dict({"text": sentences})
-    return ds_new
