bf8816592f56362ec5fb17504f44f0298311ff85
misya11p <90894519+misya11p@users.noreply.github.com>
2026-01-05 12:45:42 +0900

    update: support for inputting individual hparams to muon and adam

--- utils/_optimizer.py
+++ utils/_optimizer.py
@@ -1,21 +1,21 @@
 from muon import MuonWithAuxAdam
 
 
-def get_optimizer(model):
+def get_optimizer(model, hparams_muon=None, hparams_adam=None):
+    hparams_muon = hparams_muon or {}
+    hparams_adam = hparams_adam or {}
     params_muon = []
-    params_adamw = []
+    params_adam = []
+
     for name, parameter in model.named_parameters():
         if (parameter.ndim >= 2) and "transformer_layers." in name:
             params_muon.append(parameter)
         else:
-            params_adamw.append(parameter)
+            params_adam.append(parameter)
 
     optimizer = MuonWithAuxAdam([
-        dict(params=params_muon, use_muon=True),
-        dict(
-            params=params_adamw,
-            lr=3e-4, betas=(0.9, 0.95), weight_decay=0.01,
-            use_muon=False
-        )
+        dict(params=params_muon, use_muon=True, **hparams_muon),
+        dict(params=params_adam, use_muon=False, **hparams_adam),
     ])
+
     return optimizer
