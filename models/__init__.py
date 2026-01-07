d2cb8fbdb907974c18b4cdd6a1edb6b9b4ff9c93
misya11p <90894519+misya11p@users.noreply.github.com>
2026-01-07 18:36:41 +0900

    rename: model utils

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,3 @@
-from ._utils import get_model, load_model
 from .vanilla_transformer import VanillaTransformer
 from .gpt2 import GPT2Encoder, GPT2
 from .vision_transformer import VisionTransformerEncoder, VisionTransformer
