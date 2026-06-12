a5410446c550714eb8e013848e4a1543aef61eae
misya11p <90894519+misya11p@users.noreply.github.com>
2026-06-12 22:40:14 +0900

    pipeline

---
+++ pipelines/base.py
@@ -0,0 +1,283 @@
+import os
+from pathlib import Path
+from abc import ABC, abstractmethod
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from collections import OrderedDict
+import contextlib
+
+import torch
+import torch.distributed as dist
+from torch.nn.parallel import DistributedDataParallel as DDP
+from torch.accelerator import current_accelerator
+from torch.utils.data import DataLoader
+from transformers import get_cosine_schedule_with_warmup
+from safetensors.torch import save_file
+import wandb
+from tqdm import tqdm
+
+from utils import get_model, get_optimizer
+
+
+ROOT = Path(__file__).parent.parent
+DPATH_CHECKPOINTS = ROOT / "checkpoints"
+FNAME_STATE = "state.pth"
+FNAME_MODEL = "model.safetensors"
+CPU = torch.device("cpu")
+
+
+class BasePipeline(ABC):
+    def __init__(self, config):
+        self.config = config
+        self.device = current_accelerator(check_available=True) or CPU
+        self.model = get_model(config.model)
+        if not self.is_dist(): # Prevent the same model from being placed on multiple devices
+            self.model.to(self.device)
+        self.n_params = sum(p.numel() for p in self.model.parameters())
+
+    def setup_train(self, dpath_ckpt=None):
+        self.start_time = datetime.now(tz=ZoneInfo("Asia/Tokyo"))
+        config_train = self.config_train # alias
+
+        self.total_steps = config_train.total_steps
+        self.grad_accum_steps = config_train.grad_accum_steps
+        scheduler_steps = config_train.total_steps // config_train.grad_accum_steps
+        warmup_steps = int(config_train.warmup_ratio * scheduler_steps)
+        self.max_grad_norm = config_train.max_grad_norm
+
+        self.optimizer = get_optimizer(
+            self.model,
+            hparams_adam=config_train.adam,
+            hparams_muon=config_train.muon
+        )
+        self.scheduler = get_cosine_schedule_with_warmup(
+            self.optimizer,
+            num_warmup_steps=warmup_steps,
+            num_training_steps=scheduler_steps,
+        )
+        self.scaler = torch.amp.GradScaler()
+
+        self.now_steps = 0
+        self._setup_checkpoint(dpath_ckpt)
+        self.is_dist = self._is_dist()
+        self._setup_device()
+        self.is_master = self.global_rank == 0
+        self.context_autocast = torch.autocast(
+            device_type=self.device.type,
+            dtype=torch.bfloat16,
+        )
+
+        self.model = torch.compile(self.model)
+
+        self.train_loader, self.valid_loader = self.get_dataloader()
+        self.log_interval = config_train.log_interval
+        self.eval_interval = config_train.eval_interval
+        self.save_interval = config_train.save_interval
+
+        if self.is_master:
+            print(f"Model: {self.config.model.name}")
+            print(f"Number of parameters: {self.n_params:,}")
+            print(f"Number of devices: {self.world_size}")
+            if self.resume:
+                print(f"Resumed from checkpoint at {self.dpath_ckpt}")
+            else:
+                print(f"Checkpoints will be saved to {self.dpath_ckpt}")
+
+            if config_train.wandb_run:
+                name = config_train.wandb_run
+            else:
+                name = (
+                    f"[{self.config.model.name} {self.n_params // 1_000_000}M] "
+                    f"{self.start_time.strftime('%Y-%m-%d %H:%M')}"
+                )
+
+            self.wandb_run = wandb.init(
+                project=config_train.wandb_project,
+                name=name,
+                config=self.config,
+            )
+            # todo: define metrics
+
+    def _setup_checkpoint(self, dpath_ckpt):
+        if dpath_ckpt is None:
+            datestr = self.start_time.strftime("%Y%m%d_%H%M%S")
+            dpath_ckpt = DPATH_CHECKPOINTS / datestr
+            dpath_ckpt.mkdir(parents=True, exist_ok=True)
+            self.resume = False
+        else:
+            dpath_ckpt = Path(dpath_ckpt)
+            assert dpath_ckpt.exists(),\
+                f"The checkpoint directory {dpath_ckpt} does not exist."
+
+            latest_ckpt = dpath_ckpt / FNAME_STATE
+            state_dict = torch.load(latest_ckpt, map_location=CPU)
+            self.model.load_state_dict(state_dict["model"])
+            self.optimizer.load_state_dict(state_dict["optimizer"])
+            self.scheduler.load_state_dict(state_dict["scheduler"])
+            self.scaler.load_state_dict(state_dict["scaler"])
+            self.now_steps = state_dict["now_steps"]
+            # self.now_tokens = state_dict["now_tokens"]
+            self.resume = True
+        self.dpath_ckpt = dpath_ckpt
+
+    @staticmethod
+    def _is_dist():
+        world_size = os.environ.get("WORLD_SIZE", 0)
+        return (
+            dist.is_available()
+            and torch.cuda.is_available()
+            and int(world_size) >= 2
+        )
+
+    def _setup_device(self):
+        if self.is_dist:
+            rank = int(os.environ["LOCAL_RANK"])
+            self.global_rank = int(os.environ["RANK"])
+            torch.accelerator.set_device_index(rank)
+            acc = torch.accelerator.current_accelerator()
+            backend = torch.distributed.get_default_backend_for_device(acc)
+            dist.init_process_group(backend)
+            self.world_size = dist.get_world_size()
+            self.device = torch.device(rank)
+            self.model = self.model.to(self.device)
+            self.model = DDP(self.model, device_ids=[rank])
+        else:
+            self.world_size = 1
+            self.global_rank = 0
+            self.model = self.model.to(self.device)
+
+    def _get_dataloader(self):
+        ds_train, ds_valid, cls_ds = self.get_dataset()
+        ds_train = ds_train.shuffle(buffer_size=10000)
+
+        if (self.world_size >= 2) and (self.global_rank is not None):
+            ds_train = ds_train.shard(num_shards=self.world_size, index=self.global_rank)
+            ds_valid = ds_valid.shard(num_shards=self.world_size, index=self.global_rank)
+
+        train_loader = DataLoader(
+            cls_ds(ds_train),
+            batch_size=self.batch_size,
+            pin_memory=True,
+        )
+        valid_loader = DataLoader(
+            cls_ds(ds_valid),
+            batch_size=self.batch_size,
+            pin_memory=True,
+        )
+        return train_loader, valid_loader
+
+    def train(self):
+        if self.is_master:
+            print("Training started.", flush=True)
+        self.model.train()
+
+        is_running = True
+        pbar = tqdm(total=self.total_steps, disable=not self.is_master)
+        pbar.n = self.now_steps
+        pbar.refresh()
+
+        while is_running:
+            for batch in self.train_loader:
+                pbar.update()
+                self.now_steps += 1
+                is_last = self.now_steps >= self.total_steps
+                if is_last:
+                    is_updating_step = True
+                    is_logging_step = True
+                    is_evaluating_step = True
+                    is_saving_step = self.save_interval is not None
+                else:
+                    is_updating_step = self.now_steps % self.grad_accum_steps == 0
+                    is_logging_step = self.now_steps % self.log_interval == 0
+                    is_evaluating_step = self.now_steps % self.eval_interval == 0
+                    is_saving_step = (
+                        self.save_interval
+                        and self.now_steps % self.save_interval == 0
+                    )
+
+                if (not is_updating_step) and self.is_dist:
+                    context_nosync = self.model.no_sync()
+                else:
+                    context_nosync = contextlib.nullcontext()
+
+                with context_nosync, self.context_autocast:
+                    loss = self.forward(batch)
+                loss_scaled = self.scaler.scale(loss / self.grad_accum_steps)
+                loss_scaled.backward()
+
+                if is_updating_step:
+                    self.scaler.unscale_(self.optimizer)
+                    torch.nn.utils.clip_grad_norm_(
+                        self.model.parameters(), self.max_grad_norm
+                    )
+                    self.scaler.step(self.optimizer)
+                    self.scaler.update()
+                    self.optimizer.zero_grad()
+                    self.scheduler.step()
+
+                # if is_logging_step:
+                #     loss = self._reduce(loss.detach())
+                #     if self.is_master:
+                #         ppl = torch.exp(loss).item()
+                #         self.wandb_run.log(
+                #             {
+                #                 "train/perplexity": ppl,
+                #                 "total_tokens": self.now_tokens,
+                #             },
+                #             step=self.now_steps,
+                #         )
+                #         self._save_checkpoint()
+
+                # if is_evaluating_step:
+                #     ppl = self._evaluate()
+                #     if self.is_master:
+                #         self.wandb_run.log(
+                #             {
+                #                 "valid/perplexity": ppl,
+                #                 "total_tokens": self.now_tokens,
+                #             },
+                #             step=self.now_steps,
+                #         )
+                #         self._save_checkpoint()
+
+                if is_saving_step:
+                    if self.is_master:
+                        self._save_checkpoint(snapshot=True)
+
+                if is_last:
+                    is_running = False
+                    break
+
+        if self.is_master:
+            print("Training finished.", flush=True)
+            self.wandb_run.finish()
+
+    def _save_checkpoint(self, snapshot=False):
+        model_state_dict = self.model.state_dict()
+        correct_model_state_dict = OrderedDict()
+        for key, value in model_state_dict.items():
+            key = key.replace("_orig_mod.", "")
+            key = key.replace("module.", "")
+            correct_model_state_dict[key] = value
+
+        save_file(correct_model_state_dict, self.dpath_ckpt / FNAME_MODEL)
+
+        state_dict = {
+            "model": correct_model_state_dict,
+            "now_steps": self.now_steps,
+            "now_tokens": self.now_tokens,
+            "config": self.config,
+            "optimizer": self.optimizer.state_dict(),
+            "scheduler": self.scheduler.state_dict(),
+            "scaler": self.scaler.state_dict(),
+        }
+        torch.save(state_dict, self.dpath_ckpt / FNAME_STATE)
+
+        if snapshot:
+            fname_snapshot = f"{self.now_steps:0{len(str(self.total_steps))}d}.pth"
+            fpath_snapshot = self.dpath_ckpt / fname_snapshot
+            torch.save(state_dict, fpath_snapshot)
+
+    @abstractmethod
+    def forward(self, batch):
+        pass
