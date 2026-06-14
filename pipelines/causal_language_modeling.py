384d207caf8fa9297841f12929d522bf25202e28
misya11p <90894519+misya11p@users.noreply.github.com>
2026-06-14 13:49:24 +0900

    add infer method to CusalLanguageModelingPipeline

--- pipelines/causal_language_modeling.py
+++ pipelines/causal_language_modeling.py
@@ -104,3 +104,45 @@ class CausalLanguageModelingPipeline(BasePipeline):
             "valid/perplexity": ppl.item(),
         }
         return result
+
+    @torch.inference_mode()
+    def infer(
+        self,
+        start_text,
+        streaming=False,
+        max_len=100,
+        temperature=1.0,
+        top_k=5
+    ):
+        self.model.eval()
+        tokenizer = self.get_tokenizer(self.config.additional.tokenizer)
+        eos_id = tokenizer.eos_token_id
+        token_ids = tokenizer.encode(start_text, add_special_tokens=False)
+        token_ids.insert(0, eos_id)
+
+        if streaming:
+            print(start_text, end="")
+
+        for _ in range(max_len):
+            input_ids = torch.tensor(
+                token_ids[-max_len:],
+                device=self.device
+            ).unsqueeze(0)
+
+            outputs = self.model(input_ids)
+            logits = outputs[:, -1, :] / temperature
+            top_k_logits, top_k_indices = torch.topk(logits, top_k)
+            probabilities = torch.softmax(top_k_logits, dim=-1)
+            next_token = top_k_indices[0, torch.multinomial(probabilities, 1)]
+            next_token = next_token.item()
+
+            token_ids.append(next_token)
+            if next_token == eos_id:
+                break
+            if streaming:
+                print(tokenizer.convert_ids_to_tokens(next_token), end="")
+
+        generated_text = tokenizer.decode(
+            token_ids, skip_special_tokens=True
+        )
+        return generated_text
